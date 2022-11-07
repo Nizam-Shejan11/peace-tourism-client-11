1ca2ba46138aabd5f2e10b8cf3ba8b3d037182a0
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-11-08 03:51:10 +0600

    initial setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw7DikaQLtH7CHVUALf2YrOOjce70BJMA",
+  authDomain: "peace-tourism-26f52.firebaseapp.com",
+  projectId: "peace-tourism-26f52",
+  storageBucket: "peace-tourism-26f52.appspot.com",
+  messagingSenderId: "888358977254",
+  appId: "1:888358977254:web:91f3f6f917875479000527",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
