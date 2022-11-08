0845468582891f66294a2ed2ab906279c0b87fe9
Nizam-Shejan11 <www.nizam.shejan11@gmail.com>
2022-11-08 16:27:21 +0600

    navbar added

---
+++ src/Login/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h3>This is SignUp page</h3>
+    </div>
+  );
+};
+
+export default SignUp;
+<h3>This is SignUp page</h3>;
